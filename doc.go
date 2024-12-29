0f5c52d072d2f4d46f1cca2009f054d840f23e4f
jcalabro <jamesrcalabro@gmail.com>
2024-12-29 16:11:14 -0500

    remove doc.go since that's no longer needed since godoc -> pkg.go.dev

--- doc.go
+++
@@ -1,4 +0,0 @@
-// Package gt ("go types") contains various small, simple helper utility types that I find useful for writing Go programs quickly.
-//
-// They're not meant to be the fastest possible implementations, but are instead intended to make writing reliable Go code as quick and easy as possible.
-package gt
