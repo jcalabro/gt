22978dbe9b2e7a3a97224918ca92e9873f654a86
jcalabro <jamesrcalabro@gmail.com>
2024-12-29 16:02:52 -0500

    add doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package gt ("go types") contains various small, simple helper utility types that I find useful for writing Go programs quickly.
+//
+// They're not meant to be the fastest possible implementation, but are instead intended to make writing reliable Go code as quick and easy as possible.
+package gt
