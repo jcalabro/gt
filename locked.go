d4c5c58d2be3481fdbbb163ba3da8c5b273065ec
jcalabro <jamesrcalabro@gmail.com>
2025-02-04 09:10:15 -0500

    add Locked[T]

---
+++ locked.go
@@ -0,0 +1,43 @@
+package gt
+
+import "sync"
+
+// Locked wraps access to underlying data in a mutex such that retrieving the
+// data takes the mutex, then returning the data releases the mutex.
+type Locked[T any] struct {
+	mu   *sync.RWMutex
+	data T
+}
+
+// Returns a new Locked[T] that's ready for use
+func NewLocked[T any](data T) Locked[T] {
+	return Locked[T]{
+		mu:   &sync.RWMutex{},
+		data: data,
+	}
+}
+
+// Takes an exclusive lock and sets the data to the given value
+func (l *Locked[T]) Set(data T) {
+	l.mu.Lock()
+	l.data = data
+	l.mu.Unlock()
+}
+
+// Acquires an exclusive lock on the data
+func (l *Locked[T]) Get() (data T, unlock func()) {
+	l.mu.Lock()
+
+	data = l.data
+	unlock = func() { l.mu.Unlock() }
+	return
+}
+
+// Acquires a shared lock on the data
+func (l *Locked[T]) RGet() (data T, unlock func()) {
+	l.mu.RLock()
+
+	data = l.data
+	unlock = func() { l.mu.RUnlock() }
+	return
+}
---
+++ locked_test.go
@@ -0,0 +1,58 @@
+package gt_test
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+
+	"github.com/jcalabro/gt"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLocked(t *testing.T) {
+	val := 123
+	l := gt.NewLocked(val)
+	require.NotNil(t, l)
+
+	{
+		// test exclusive lock get
+		item, unlock := l.Get()
+		require.Equal(t, val, item)
+		unlock()
+	}
+
+	// test set
+	val = 456
+	l.Set(456)
+
+	{
+		// test shared lock get
+		item, unlock := l.RGet()
+		require.Equal(t, val, item)
+		unlock()
+	}
+
+	// this will trigger the race detector reasonably reliably
+	// if there is a race condition
+	wg := &sync.WaitGroup{}
+	for ndx := 0; ndx < 1000; ndx++ {
+		wg.Add(1)
+		go concurrentLockOperations(wg, &l)
+	}
+	wg.Wait()
+}
+
+func concurrentLockOperations(wg *sync.WaitGroup, l *gt.Locked[int]) {
+	defer wg.Done()
+
+	for ndx := 0; ndx < 100; ndx++ {
+		val := 100
+		l.Set(rand.Intn(val))
+
+		_, unlock1 := l.Get()
+		unlock1()
+
+		_, unlock2 := l.RGet()
+		unlock2()
+	}
+}
