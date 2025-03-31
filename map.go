2a9a51b5d3d0fc121fe2a4fca73adaa83930c6f1
jcalabro <jamesrcalabro@gmail.com>
2025-03-31 07:23:56 -0700

    map cleanup

--- map.go
+++ map.go
@@ -9,35 +9,32 @@ import (
 // and instead `NewSafeMap` should always be used.
 type SafeMap[K comparable, V any] struct {
 	items map[K]V
-	mu    *sync.RWMutex
+	mu    sync.RWMutex
 }
 
 // Initializes a new SafeMap that's ready for use
 func NewSafeMap[K comparable, V any]() SafeMap[K, V] {
 	return SafeMap[K, V]{
 		items: map[K]V{},
-		mu:    &sync.RWMutex{},
 	}
 }
 
 // Adds an item to the map, overwriting any existing value, if any
 func (m *SafeMap[K, V]) Put(key K, val V) {
 	m.mu.Lock()
-	defer m.mu.Unlock()
-
 	m.items[key] = val
+	m.mu.Unlock()
 }
 
 // Retrives the value from the map for the entry with the given key,
 // if any. Returns `None` if the item does not exist in the map.
 func (m *SafeMap[K, V]) Get(key K) Option[V] {
 	m.mu.RLock()
-	defer m.mu.RUnlock()
-
 	item, ok := m.items[key]
+	m.mu.RUnlock()
+
 	if ok {
 		return Some(item)
 	}
-
 	return None[V]()
 }
